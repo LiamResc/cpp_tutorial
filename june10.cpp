375c81d60d149b8efe554ece9057242df53a5efe
LiamResc <lreschke@ualberta.ca>
2026-06-10 20:55:10 -0600

    prints and such

---
+++ june10.cpp
@@ -0,0 +1,9 @@
+#include <iostream> // import library
+using namespace std;
+
+int main() {
+  cout << "Hello World!" << "croenem" << "\n"; // you can do these in succession
+  cout << "Croenem" << " is a cool name" << endl;
+  cout << "this is a new line"; // endl is an object that represents a new line. It also flushes the output buffer, which means it forces the output to be written to the console immediately. This can be useful in certain situations, such as when you want to ensure that all output is displayed before the program continues executing.
+  return 0;
+}
