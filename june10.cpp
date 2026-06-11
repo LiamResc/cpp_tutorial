46ff7f00287517feff9d0abf3a593c2cd48cebbc
LiamResc <lreschke@ualberta.ca>
2026-06-10 21:05:44 -0600

    messing around around with some semantics

--- june10.cpp
+++ june10.cpp
@@ -4,6 +4,12 @@ using namespace std;
 int main() {
   cout << "Hello World!" << "croenem" << "\n"; // you can do these in succession
   cout << "Croenem" << " is a cool name" << endl;
-  cout << "this is a new line"; // endl is an object that represents a new line. It also flushes the output buffer, which means it forces the output to be written to the console immediately. This can be useful in certain situations, such as when you want to ensure that all output is displayed before the program continues executing.
-  return 0;
-}
+  cout << "this is a new line \n"; // endl is an object that represents a new line. 
+
+  int x, y, z = 10; // you can declare multiple variables of the same type in one line. But 
+  // only z is initialized to 10, x and y are uninitialized and will have garbage values.
+  cout << "Heres some BS:" << (x + y + z) << endl;// some BS
+  
+  x = 10, y = 10;
+  cout << x + y + z; // 30
+    return 0;}
